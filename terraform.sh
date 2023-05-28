8ca37b448b537fa3bbf810d6ae84cfe4a332969e
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2023-05-28 12:17:12 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install unzip
+https://releases.hashicorp.com/terraform/1.4.6/terraform_1.4.6_linux_amd64.zip
+ 
+unzip terraform_1.4.6_linux_amd64.zip
 
-wget https://releases.hashicorp.com/terraform/1.0.7/terraform_1.0.7_linux_amd64.zip
-
-unzip terraform_1.0.7_linux_amd64.zip
-
-sudo mv terraform /usr/local/bin/
+ sudo mv terraform /usr/local/bin/
 
 terraform --version 
